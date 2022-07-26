a8ac09fa6b138c11094eb20559029b1e62f5a9be
mnekx <karimnembac@gmail.com>
2022-07-26 14:44:18 +0300

    Update RepaymentsComponent.js

--- client/src/features/loans/RepaymentsComponent.js
+++ client/src/features/loans/RepaymentsComponent.js
@@ -1,11 +1,13 @@
 import styleUtilities from '../../utility.module.css';
 import loanStyles from './loans.module.css';
 import repaymentStyles from './repayments.module.css';
+import NewRepayment from './NewRepayment';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const RepaymentsComponent = () => {
   const [data, setData] = useState(null);
+  const [openRepaymentForm, setOpenRepaymentForm] = useState(false);
   const { loanId } = useParams();
   useEffect(() => {
     fetch('/api/loans/' + loanId + '/repayments')
@@ -27,7 +29,7 @@ const RepaymentsComponent = () => {
         {data?.map((repayment) => {
           const repaymentDate = new Date(repayment.date);
           return (
-            <tr>
+            <tr key={repayment._id}>
               <td>{repaymentDate.toDateString()}</td>
               <td>{Number(repayment.amount).toLocaleString()}</td>
               <td>Reciever</td>
@@ -37,14 +39,20 @@ const RepaymentsComponent = () => {
       </tbody>
     </table>
   );
+  const openRepayForm = () => {
+    setOpenRepaymentForm((prevState) => !prevState);
+  };
   return (
     <section
       className={`${styleUtilities.Section} ${loanStyles.Section} ${repaymentStyles.RepaymentsSection}`}
     >
       <h2>
         Repayments&nbsp;&nbsp;
-        <button className={`${styleUtilities.Btn}`}>New</button>
+        <button className={`${styleUtilities.Btn}`} onClick={openRepayForm}>
+          {openRepaymentForm ? 'Close' : 'New'}
+        </button>
       </h2>
+      <NewRepayment open={openRepaymentForm} />
       <div>
         {data ? (
           data.length === 0 ? (
