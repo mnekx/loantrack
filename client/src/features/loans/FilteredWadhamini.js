db0b3fa30509c0db954a55a13eaefccf52ba244b
mnekx <karimnembac@gmail.com>
2022-08-02 12:34:48 +0300

    Update FilteredWadhamini.js

--- client/src/features/loans/FilteredWadhamini.js
+++ client/src/features/loans/FilteredWadhamini.js
@@ -1,10 +1,21 @@
 import styles from './filtered-wadhamini.module.css';
+import styleUtilities from '../../utility.module.css';
+import { useEffect } from "react";
 
 const FilteredWadhamini = (props) => {
+
+  useEffect(() => {
+    const ul = document.querySelector('ul');
+    ul.style.top = props.pos + 170 + 'px';
+  }, [props.pos])
   return (
-    <ul className={`${styles.Ul}`}>
+    <ul className={`${styles.Ul} ${styleUtilities.DFlex}`}>
       {props.filteredList.map((customer) => (
-        <li key={customer._id}>
+        <li
+          key={customer._id}
+          className={`${styleUtilities.DFlex} ${styles.Li}`}
+          onClick={() => props.onClickExistingMdhamini(customer._id)}
+        >
           {customer.firstName} {customer.lastName} - {customer.phone}{' '}
           <img
             src={'http://localhost:3001/pics/' + customer.image}
